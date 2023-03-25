374be03465c16366239bb6cebb896b644719629f
wang <2647006063@qq.com>
2023-03-25 18:38:05 +0800

    第一次提交代码

---
+++ Manager/InfoManager/DataAccess/UserDataManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+namespace InfoManager.DataAccess
+{
+    public class UserDataManager
+    {
+        public void xmlAccess(string UserName, string PassWord, string Num)
+        {
+            string Path = "D:\\ProgramData\\";
+            string fullPath = Path + Guid.NewGuid().ToString() + ".xml";
+            if (!File.Exists(fullPath))
+            {
+                XmlDocument xml = new XmlDocument();
+                XmlElement config = xml.CreateElement("root");
+                xml.AppendChild(config);
+                XmlElement user = xml.CreateElement("UserName");
+                user.InnerText = UserName;
+                XmlElement password = xml.CreateElement("PassName");
+                password.InnerText = PassWord;
+                XmlElement num = xml.CreateElement("Num");
+                num.InnerText = Num;
+
+                config.AppendChild(user);
+                config.AppendChild(password);
+                config.AppendChild(num);
+
+                xml.Save(fullPath);
+            }
+            else
+            {
+                File.Delete(fullPath);
+                XmlDocument xml = new XmlDocument();
+                XmlElement config = xml.CreateElement("root");
+                xml.AppendChild(config);
+                XmlElement user = xml.CreateElement("UserName");
+                user.InnerText = UserName;
+                XmlElement password = xml.CreateElement("PassName");
+                password.InnerText = PassWord;
+                XmlElement num = xml.CreateElement("Num");
+                num.InnerText = Num;
+
+                config.AppendChild(user);
+                config.AppendChild(password);
+                config.AppendChild(num);
+
+                xml.Save(fullPath);
+            }
+        }
+    }
+}
---
+++ Manager/InfoManager/View/Login.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml;
+using InfoManager.ViewModel;
+using InfoManager.DataAccess;
+namespace InfoManager.View
+{
+    /// <summary>
+    /// Login.xaml 的交互逻辑
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+            this.DataContext = new LoginViewModel();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            UserDataManager userDataManager = new UserDataManager();
+            userDataManager.xmlAccess(UserName.Text, PassWord.Text, Num.Text);
+        }
+    }
+}
