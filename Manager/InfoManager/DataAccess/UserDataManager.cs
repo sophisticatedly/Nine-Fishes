2bfa273adecb09192643553a57c4c7c6a3a4d46b
wang <2647006063@qq.com>
2023-03-26 22:46:39 +0800

    修改代码

--- Manager/InfoManager/DataAccess/UserDataManager.cs
+++ Manager/InfoManager/DataAccess/UserDataManager.cs
@@ -8,44 +8,90 @@ namespace InfoManager.DataAccess
         public void xmlAccess(string UserName, string PassWord, string Num)
         {
             string Path = "D:\\ProgramData\\";
-            string fullPath = Path + Guid.NewGuid().ToString() + ".xml";
-            if (!File.Exists(fullPath))
+            try 
             {
-                XmlDocument xml = new XmlDocument();
-                XmlElement config = xml.CreateElement("root");
-                xml.AppendChild(config);
-                XmlElement user = xml.CreateElement("UserName");
+                if (!File.Exists(Path + "infoList.xml"))
+                {
+                    XmlDocument xml = new XmlDocument();
+                    XmlElement config = xml.CreateElement("root");
+                    xml.AppendChild(config);
+                    XmlElement User = xml.CreateElement("UserName");
+                    User.InnerText = UserName;
+                    XmlElement Password = xml.CreateElement("PassName");
+                    Password.InnerText = PassWord;
+                    XmlElement Nm = xml.CreateElement("Num");
+                    Nm.InnerText = Num;
+
+                    config.AppendChild(User);
+                    config.AppendChild(Password);
+                    config.AppendChild(Nm);
+
+                    xml.Save(Path + "infoList.xml");
+                }
+                XmlDocument xmldocument = new XmlDocument();
+                xmldocument.Load(Path + "infoList.xml");
+                XmlNode root = xmldocument.SelectSingleNode("root");
+                
+               
+                XmlElement user = xmldocument.CreateElement("UserName");
                 user.InnerText = UserName;
-                XmlElement password = xml.CreateElement("PassName");
+                root.AppendChild(user);
+
+                XmlElement password = xmldocument.CreateElement("PassName");
                 password.InnerText = PassWord;
-                XmlElement num = xml.CreateElement("Num");
+                root.AppendChild(password);
+
+                XmlElement num = xmldocument.CreateElement("Num");
                 num.InnerText = Num;
+                root.AppendChild(num);
 
-                config.AppendChild(user);
-                config.AppendChild(password);
-                config.AppendChild(num);
 
-                xml.Save(fullPath);
-            }
-            else
+                xmldocument.Save(Path + "infoList.xml");
+
+                string fullPath = Path + Guid.NewGuid().ToString() + ".xml";
+                if (!File.Exists(fullPath))
+                {
+                    XmlDocument xml2 = new XmlDocument();
+                    XmlElement config2 = xml2.CreateElement("root");
+                    xml2.AppendChild(config2);
+                    XmlElement user2 = xml2.CreateElement("UserName");
+                    user2.InnerText = UserName;
+                    XmlElement password2 = xml2.CreateElement("PassName");
+                    password2.InnerText = PassWord;
+                    XmlElement num2 = xml2.CreateElement("Num");
+                    num2.InnerText = Num;
+
+                    config2.AppendChild(user2);
+                    config2.AppendChild(password2);
+                    config2.AppendChild(num2);
+
+                    xml2.Save(fullPath);
+                }
+                else
+                {
+                    File.Delete(fullPath);
+                    XmlDocument xml2 = new XmlDocument();
+                    XmlElement config2 = xml2.CreateElement("root");
+                    xml2.AppendChild(config2);
+                    XmlElement user2 = xml2.CreateElement("UserName");
+                    user2.InnerText = UserName;
+                    XmlElement password2 = xml2.CreateElement("PassName");
+                    password2.InnerText = PassWord;
+                    XmlElement num2 = xml2.CreateElement("Num");
+                    num2.InnerText = Num;
+
+                    config2.AppendChild(user2);
+                    config2.AppendChild(password2);
+                    config2.AppendChild(num2);
+
+                    xml2.Save(fullPath);
+                }
+            } 
+            catch 
             {
-                File.Delete(fullPath);
-                XmlDocument xml = new XmlDocument();
-                XmlElement config = xml.CreateElement("root");
-                xml.AppendChild(config);
-                XmlElement user = xml.CreateElement("UserName");
-                user.InnerText = UserName;
-                XmlElement password = xml.CreateElement("PassName");
-                password.InnerText = PassWord;
-                XmlElement num = xml.CreateElement("Num");
-                num.InnerText = Num;
-
-                config.AppendChild(user);
-                config.AppendChild(password);
-                config.AppendChild(num);
-
-                xml.Save(fullPath);
+                throw;
             }
+            
         }
     }
 }
