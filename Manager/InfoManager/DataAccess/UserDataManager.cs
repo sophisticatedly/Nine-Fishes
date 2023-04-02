f4bd7d0bb88961b4acab8370808d4e0c4d240a4b
wang <2647006063@qq.com>
2023-04-02 17:34:57 +0800

    e

--- Manager/InfoManager/DataAccess/UserDataManager.cs
+++ Manager/InfoManager/DataAccess/UserDataManager.cs
@@ -87,6 +87,7 @@ namespace InfoManager.DataAccess
                     xml2.Save(fullPath);
                 }
             } 
+            //test pull of git
             catch (Exception e)
             {
                 throw e;
